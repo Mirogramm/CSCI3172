ddaae94e903934959fb54e8359702f3370e15b9d
Mirgani Kheir <mr859867@dal.ca>
2025-01-17 23:39:59 -0400

    Fixed w3validate

--- lab1/index.php
+++ lab1/index.php
@@ -18,14 +18,14 @@
     <p>Hey my name is Mirgani Kheir but you can call me Mir. What can I call you?</p>
     <form>
         <label for="first-name">First name</label>
-        <input type="text" id="infirst-name" name="first-name" required><br>
+        <input type="text" id="first-name" name="first-name" required><br>
         <button type="submit" id="name-button">Click here</button>
     </form>
 
     <script>
         var button = document.getElementById("name-button");
         button.addEventListener("click", function() {
-            var fname = document.getElementById("infirst-name").value;
+            var fname = document.getElementById("first-name").value;
             alert("Hello " + fname + " nice to meet you!");
         });
     </script>
