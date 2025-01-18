2d085e8e9dcc87432b127f42f47e3bda9e182f0f
Mirgani Kheir <mr859867@dal.ca>
2025-01-17 23:34:19 -0400

    Lab1 work

--- lab1/index.php
+++ lab1/index.php
@@ -8,5 +8,25 @@
 </head>
 
 <body>
-    <h1>I'm not a good programmer</h1>
+    <nav>
+        <a href="index.php">
+            <!-- Made by myself nothing to source -->
+            <img src="img/peacem.png" alt="logo" height="50">
+        </a>
+    </nav>
+    <h1>I'm not a good programmer!</h1>
+    <p>Hey my name is Mirgani Kheir but you can call me Mir. What can I call you?</p>
+    <form>
+        <label for="first-name">First name</label>
+        <input type="text" id="infirst-name" name="first-name" required><br>
+        <button type="submit" id="name-button">Click here</button>
+    </form>
+
+    <script>
+        var button = document.getElementById("name-button");
+        button.addEventListener("click", function() {
+            var fname = document.getElementById("infirst-name").value;
+            alert("Hello " + fname + " nice to meet you!");
+        });
+    </script>
 </body>
